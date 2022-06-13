1a1d68f8262b08c3dbeeb25f43114b4278e54fae
Corey96 <0coreyy@gmail.com>
2022-06-13 20:56:55 +0100

    further model work and created routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,27 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reationBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+
+    username: {
+        type: String,
+        required: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+}),
+
 
 const thoughtSchema = new Schema(
     {
